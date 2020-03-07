53ccd15261224f36b6788c2f18b889e7b8e161d3
Joshua <joshuaoliverdouglas@gmail.com>
2020-03-07 21:14:21 +0000

    User can sign in with email

--- spec/support/features/sign_in.rb
+++ spec/support/features/sign_in.rb
@@ -1,8 +1,8 @@
 module Features
   def sign_in
-    visit root_path
-    fill_in "Email", with: "person@example.com"
-    click_on "Sign in"
+   visit root_path
+   fill_in "Email", with: "person@example.com"
+   click_on "Sign in"
     #todo contorller redirects to sign in/sign up and fill out info, sign in and when i go to todos page it should work
   end
 end
