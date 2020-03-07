6fac33724340f5dde0b9f1d896a28638bd418534
Joshua <joshuaoliverdouglas@gmail.com>
2020-03-07 21:48:24 +0000

    Finished signing in and todo ownership

--- spec/support/features/sign_in.rb
+++ spec/support/features/sign_in.rb
@@ -1,8 +1,12 @@
 module Features
   def sign_in
-   visit root_path
-   fill_in "Email", with: "person@example.com"
-   click_on "Sign in"
+   sign_in_as "person@example.com"
     #todo contorller redirects to sign in/sign up and fill out info, sign in and when i go to todos page it should work
   end
+
+  def sign_in_as(email)
+    visit root_path
+    fill_in "Email", with: email
+    click_on "Sign in"
+   end
 end
